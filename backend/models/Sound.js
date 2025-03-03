94437ff87165d5f0810b7a4d2549bf0620fcddcd
Sharvan Sharma <itbhu.sk@gmail.com>
2025-03-03 11:52:12 +0530

    Automated commit: backend/models/Sound.js

---
+++ backend/models/Sound.js
@@ -0,0 +1,1 @@
+const mongoose = require('mongoose'); const soundSchema = new mongoose.Schema({ name: { type: String, required: true }, file: { type: String, required: true } }, { timestamps: true }); module.exports = mongoose.model('Sound', soundSchema);
